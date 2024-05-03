a38a8b7ecfb5074ffa32c3428d1d3179839ba434
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-05-03 13:38:06 +0800

    Add files via upload

---
+++ code/2024/4-27/install.bat
@@ -0,0 +1,10 @@
+python -m ensurepip --default-pip
+
+pip install requests
+pip install jsonpath-ng
+pip install beautifulsoup4
+pip install pillow
+pip install chardet
+pip install selenium
+
+pause
