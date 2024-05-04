95ec06058f9aedfe8ef1db52025692fac942777f
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-05-04 20:18:52 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var modal = document.createElement('div');
     modal.classList.add('modal');
-    modal.innerHTML = '<p>请在阅读 <a href="./code/README.html" target="_blank">『README相关信息(README)』</a> 之后浏览本站内容,否则因各种原因发生的任何问题的后果将全部由您承担!</p>';
+    modal.innerHTML = '<p>请在阅读 <a href="./code/README.html" target="_blank">『硫酸铝的代码站---相关说明』</a> 之后浏览本站内容,否则因各种原因发生的任何问题的后果将全部由您承担!</p>';
     document.body.appendChild(modal);
     modal.style.display = 'block';
     setTimeout(function() {
